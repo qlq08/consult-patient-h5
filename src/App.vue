2911b1d857e319380562c4959747a9b8629ea9da
权 <2071675988@qq.com>
2022-12-27 14:54:11 +0800

    vant组件库

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,12 @@ import { Button as VanButton } from 'vant'
 </script>
 <template>
   <van-button type="success">按钮</van-button>
+  <div class="box"></div>
 </template>
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+}
+</style>
