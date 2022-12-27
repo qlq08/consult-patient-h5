8c174018d17a938bbfc5f19fd43d20178747f00b
权 <2071675988@qq.com>
2022-12-27 13:09:47 +0800

    分析路由代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// 回顾: Vue2的路由
+// 1.import VueRouter from 'vue-router'
+// 2. const router=new VueRouter({routers:{ // 路由规则}})
+// 3.选择路由模式  hash /#/user     history   /user
+
+// 现在 Vue3的路由
+// 1.创建路由实例 createRouter({ // 配置对象})
+// 2.配置选项中 routers:[ // 路由规则]
+// 3.createWebHistory 使用路由history模式
+// 4.createWebHashHistory 使用路由hash模式
+// 5.import.meta.env.BASE_URL  路由的基准路由  create-vue 脚手架提供的数据(环境变量)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
