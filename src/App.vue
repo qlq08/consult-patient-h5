97fbc5c2b415732f589060ee91afe2bd01380ab6
权 <2071675988@qq.com>
2022-12-27 14:02:14 +0800

    数据持久化

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useUserStore } from './stores/user'
+
+const store = useUserStore()
+</script>
 <template>
   <div>App根组件</div>
+  <div>用户:{{ store.user }}</div>
+  <button
+    @click="
+      store.setUser({
+        id: '1',
+        mobile: '1',
+        avatar: '1',
+        account: '1',
+        token: '1'
+      })
+    "
+  >
+    登录
+  </button>
+  <button @click="store.delUser()">退出</button>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import persist from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import './styles/main.scss'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// 使用pinia插件，use(pinia的插件)
+app.use(createPinia().use(persist))
 app.use(router)
 
 app.mount('#app')
