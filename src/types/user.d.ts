2bfeeaeeaf0d9b484122d310ef37ae0592f81377
权 <2071675988@qq.com>
2022-12-28 16:15:12 +0800

    个人中心-用户信息类型

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -13,3 +13,34 @@ export type CodeType =
   | 'changeMobile'
   | 'forgetPassword'
   | 'bindMobile'
+
+// 排出 token 得到对象类型
+// 个人中心-用户信息类型
+// Omit 可以从一个对象类型中 排除某些属性
+// Pick作用?
+// 从类型对象中取出指定的属性类型
+// Omit 作用?
+// 从类型对象中排出指定的属性类型,得到剩余的
+
+// 个人信息
+type OmitUser = Omit<User, 'token'>
+type UserInfo = OmitUser & {
+  /* 关注 */
+  likeNumber: number
+  /* 收藏 */
+  collectionNumber: number
+  /* 积分 */
+  score: number
+  /* 优惠券 */
+  couponNumber: number
+  orderInfo: {
+    /* 待付款 */
+    paidNumber: number
+    /* 待发货 */
+    receivedNumber: number
+    /* 待收货 */
+    shippedNumber: number
+    /* 已完成 */
+    finishedNumber: number
+  }
+}
