b3ede1861f12806327667dbf6c7e95d3e3424971
权 <2071675988@qq.com>
2022-12-28 15:01:46 +0800

    短信登录-进行登录

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="layout-page">
+    <!-- 二级路由出口  home  article notify user 等组件 -->
+    <router-view />
+  </div>
+</template>
+<style lang="scss" scoped></style>
