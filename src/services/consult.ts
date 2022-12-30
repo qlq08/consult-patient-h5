5d6d652269c34eaca510550bcdc2a307e57f9233
权 <2071675988@qq.com>
2022-12-30 22:33:03 +0800

    首页模块-推荐关注医生-展示

--- src/services/consult.ts
+++ src/services/consult.ts
@@ -1,6 +1,15 @@
-import type { KnowledgePage, KnowledgeParams } from '@/types/consult'
+import type {
+  KnowledgePage,
+  KnowledgeParams,
+  DoctorPage,
+  PageParams
+} from '@/types/consult'
 
 import { request } from '@/utils/request'
 
 export const getKnowledgePage = (params: KnowledgeParams) =>
   request<KnowledgePage>('/patient/home/knowledge', 'GET', params)
+
+// 推荐关注医生
+export const getDoctorPage = (params: PageParams) =>
+  request<DoctorPage>('home/page/doc', 'GET', params)
